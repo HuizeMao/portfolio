3332f918d7ae21ed1312248569cf320af3d19654
HuizeMao <Huizemao@gmail.com>
2025-04-17 11:43:50 -0700

    feat: JS file + automatic nav menu

---
+++ global.js
@@ -0,0 +1,53 @@
+// // Step 2.1: Get all nav links
+// const navLinks = $$("nav a");
+
+// // Step 2.2: Find the link to the current page
+// const currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname
+// );
+
+// // Step 2.3: Add the class "current" to that link
+// currentLink?.classList.add("current");
+
+console.log("IT’S ALIVE!");
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/" 
+  : "/portfolio/";
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'https://github.com/HuizeMao', title: 'GitHub' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
+}
